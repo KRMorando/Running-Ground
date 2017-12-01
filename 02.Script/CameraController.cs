0a89d56ba381e43b8564166920ca236fdfcc4153
KRMorando <kimjy6727@naver.com>
2017-12-01 22:38:00 +0900

    Add files via upload

---
+++ 02.Script/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject targetCamera;
+	public GameObject targetObject;
+
+	private Vector3 offset; //카메라와 타겟 사의 거리
+
+	// Use this for initialization
+	void Start () {
+		offset = targetCamera.transform.position - targetObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		targetCamera.transform.position = offset + targetObject.transform.position;
+	}
+}
---
+++ 02.Script/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+	private Rigidbody rigidbody;
+
+	private float fDestroyTime = 2f;
+	private float fTickTime;
+
+	void Start() {
+		rigidbody = GetComponent<Rigidbody>();
+	}
+
+	void Update() {
+		fTickTime += Time.deltaTime;
+
+		if ( fTickTime >= fDestroyTime) {
+				Vector3 up = new Vector3 (0f, 0.0f, 1f);
+
+				rigidbody.AddForce (up);
+		}
+	}
+}
